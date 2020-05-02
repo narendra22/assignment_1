c7b047611ae0ffd8608ebd2650c2c117428d7644
Narendra <narendra.leander22@gmail.com>
2020-05-02 18:43:58 +0530

    assignment1

---
+++ assignment1_task2_2.py
@@ -0,0 +1,15 @@
+number = int(input('Enter a number: '))
+for i in range (0, number+1):
+    for j in range(0, i+1):
+        print('*', end=" ")
+    print() 
+for j in range(0, number):
+    for i in range(0, i):
+        print('*', end=' ')
+    print()        
+
+
+   
+    
+        
+               
---
+++ assignment1_task2_4.py
@@ -0,0 +1,2 @@
+message = 'WE, THE PEOPLE OF INDIA, \n + '   ' having solemnly resolved to constitute India into a SOVEREIGN, ! \n SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC and to secure to all \n its citizens'
+print(message)
